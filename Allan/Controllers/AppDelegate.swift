f0faeac66eede57b948b28ec000423b0ea497efe
Tobias Sundstrand <tobias.sundstrand@gmail.com>
2015-02-28 19:57:45 +0100

    Added

--- Allan/Controllers/AppDelegate.swift
+++ Allan/Controllers/AppDelegate.swift
@@ -3,4 +3,11 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        CBLManager.sharedInstance()
+        return true
+    }
+
 }
+
--- Podfile
+++ Podfile
@@ -1,6 +1,7 @@
 platform :ios, '8.0'
 
 # Add Application pods here
+pod 'couchbase-lite-ios', '~> 1.0.3.1'
 
 target :unit_tests, :exclusive => true do
   link_with 'UnitTests'
